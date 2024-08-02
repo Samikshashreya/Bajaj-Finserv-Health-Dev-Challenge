b0d1c32392a742aa929b23c57ea235ed35076138
SamikshaShreya <samiksha1105@gmail.com>
2024-08-02 22:28:26 +0530

    fix error

--- bfhl-frontend/src/App.js
+++ bfhl-frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
       setError('');
       // Parse JSON and submit
       const parsedData = JSON.parse(jsonData);
-      const res = await axios.post('http://localhost:3000/bfhl', parsedData);
+      const res = await axios.post('http://localhost:4000/bfhl', parsedData);
       setResponse(res.data);
     } catch (error) {
       // Set error message if JSON parsing fails or if there's an API error
